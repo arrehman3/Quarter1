ed9704fb29a30aa6d441721b790355c99b183ac7
Abdur Rehman <abdurrahman988.ar@gmail.com>
2023-08-15 23:02:46 +0500

    adding dev dependencies,dependencies

---
+++ Quiz/quiz.ts
@@ -0,0 +1,15 @@
+// quiz.....incquirer,chalk,node-banner
+//-D for developement,types for all types recogniction
+//npm i @types/node -D
+//npm i @types/chalk -D
+/*npm i @types/inquirer -D
+node banner no types
+npm i @types/node-banner -D
+added dependencies
+inquirer dor input from user
+npm i inquirer
+types in development because only necessary in production.
+developmnet dependencies only for development
+*/
+import inquirer from 'inquirer';
+import chalk from 'chalk';
